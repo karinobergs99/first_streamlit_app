497efdff9f400d318b2c924d95af8c6d41bf7e8b
karinobergs99 <146738887+karinobergs99@users.noreply.github.com>
2023-10-02 15:37:25 +0200

    Three items on the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('My parents healthy dinner')
 
 streamlit.header('Breakfast menu')
-streamlit.text('Cookies')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
